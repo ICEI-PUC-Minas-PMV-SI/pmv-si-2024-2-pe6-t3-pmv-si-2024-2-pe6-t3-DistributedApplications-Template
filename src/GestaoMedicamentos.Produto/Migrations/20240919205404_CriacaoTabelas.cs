9c6b59af62935ba2b5327114874ec9e2b2c72ef4
WallaceSousa88 <wallaceinaciogs@gmail.com>
2024-09-19 21:08:31 +0000

    add bd farmacia_hospital

---
+++ src/GestaoMedicamentos.Produto/Migrations/20240919205404_CriacaoTabelas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoMedicamentos.Produto.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
