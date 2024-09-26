c4b320f567830efca52eb6a84c8046900a776771
WallaceSousa88 <wallaceinaciogs@gmail.com>
2024-09-26 17:19:19 +0000

    upd UsuariosController.cs

--- src/Estoque.Farmacia.API/Controllers/UsuariosController.cs
+++ src/Estoque.Farmacia.API/Controllers/UsuariosController.cs
@@ -12,7 +12,7 @@ namespace Estoque.Farmacia.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
+    //[Authorize]
     public class UsuariosController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -52,7 +52,7 @@ namespace Estoque.Farmacia.API.Controllers
             _context.Usuarios.Add(usuario);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("ObterPorId", new { id = usuario.Id }, usuario);
+            return CreatedAtRoute("ObterUsuario", new { id = usuario.Id }, usuario);
         }
 
         // GET: api/Usuarios
