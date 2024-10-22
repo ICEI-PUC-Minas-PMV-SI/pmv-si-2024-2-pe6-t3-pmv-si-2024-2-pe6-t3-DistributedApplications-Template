876647ab47fdd97e9c1b2ada74f07c7458f9859c
henriquepaulalima <henrique.p.lima0@gmail.com>
2024-10-22 11:31:01 -0300

    Add CORS policy

--- src/Estoque.Farmacia.API/Program.cs
+++ src/Estoque.Farmacia.API/Program.cs
@@ -11,6 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy => policy
+            .WithOrigins("*")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -49,6 +58,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
