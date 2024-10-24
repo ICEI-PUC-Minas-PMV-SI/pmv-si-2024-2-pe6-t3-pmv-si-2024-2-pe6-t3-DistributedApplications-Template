f1b45ff493f31030fef6000db6766eeb63421436
henriquepaulalima <henrique.p.lima0@gmail.com>
2024-10-24 11:48:11 -0300

    Update get-report

---
+++ src/estoque.farmacia.web/src/app/get-report/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import styles from './page.module.scss';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Dayjs } from 'dayjs';
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+export default function GetReport() {
+  const [startDate, setStartDate] = useState<Dayjs | null>(null);
+  const [startDateFormated, setStartDateFormated] = useState<string | null>(
+    null
+  );
+  const [endDate, setEndtDate] = useState<Dayjs | null>(null);
+  const [endDateFormated, setEndtDateFormated] = useState<string | null>(null);
+
+  const handleStartData = (value: Dayjs | null) => {
+    if (value) {
+      const formatedNewValue = value.startOf('day');
+      setStartDate(formatedNewValue);
+      setStartDateFormated(formatedNewValue.format('l').replaceAll('/', '-'));
+    }
+  };
+
+  const handleEndData = (value: Dayjs | null) => {
+    if (value) {
+      const formatedNewValue = value.endOf('day');
+      setEndtDate(formatedNewValue);
+      setEndtDateFormated(formatedNewValue.format('l').replaceAll('/', '-'));
+    }
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <section className={styles.register_report__container}>
+        <h2>Gerar um Relatório</h2>
+        <div className={styles.register_report__dates_container}>
+          <DatePicker
+            label='Data inicial'
+            value={startDate}
+            onChange={handleStartData}
+          />
+          <DatePicker
+            label='Data final'
+            value={endDate}
+            onChange={handleEndData}
+          />
+        </div>
+        <div className={styles.register_report__submit_container}>
+          <Link href={`/report/${startDateFormated}/${endDateFormated}`}>
+            <button disabled={!startDate || !endDate}>Gerar</button>
+          </Link>
+        </div>
+      </section>
+    </LocalizationProvider>
+  );
+}
