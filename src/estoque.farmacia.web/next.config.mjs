52f4126f57197a45a824ff539d346a415b7a3f06
WALLACE INACIO GOMES DE SOUSA <wallace.inacio@cemig.com.br>
2024-11-01 08:27:19 -0300

    add endpoint

--- src/estoque.farmacia.web/next.config.mjs
+++ src/estoque.farmacia.web/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // env: {
+  //   NEXT_PUBLIC_API_URL: 'http://100.28.74.101:5298',
+  // },
   async redirects() {
     return [
       {
