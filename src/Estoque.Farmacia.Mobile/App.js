63c15fae56d4ee7535fb32ba543d03c0e005c737
henriquepaulalima <henrique.p.lima0@gmail.com>
2024-11-13 16:12:32 -0300

    Remove unused pages

--- src/Estoque.Farmacia.Mobile/App.js
+++ src/Estoque.Farmacia.Mobile/App.js
@@ -3,10 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import FornecedorScreen from './src/screens/FornecedorScreen';
-import MedicamentoScreen from './src/screens/MedicamentoScreen';
 import UsuarioScreen from './src/screens/UsuarioScreen';
-import EntradaScreen from './src/screens/EntradaScreen';
-import SaidaScreen from './src/screens/SaidaScreen';
 import LoteScreen from './src/screens/LoteScreen';
 
 const Stack = createStackNavigator();
@@ -17,10 +14,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Fornecedor" component={FornecedorScreen} />
-        <Stack.Screen name="Medicamento" component={MedicamentoScreen} />
         <Stack.Screen name="Usuario" component={UsuarioScreen} />
-        <Stack.Screen name="Entrada" component={EntradaScreen} />
-        <Stack.Screen name="Saida" component={SaidaScreen} />
         <Stack.Screen name="Lote" component={LoteScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/Estoque.Farmacia.Mobile/src/screens/HomeScreen.js
+++ src/Estoque.Farmacia.Mobile/src/screens/HomeScreen.js
@@ -6,10 +6,7 @@ const HomeScreen = ({ navigation }) => {
     <View>
       <Text>Bem-vindo ao Estoque Farmácia</Text>
       <Button title="Fornecedor" onPress={() => navigation.navigate('Fornecedor')} />
-      <Button title="Medicamento" onPress={() => navigation.navigate('Medicamento')} />
       <Button title="Usuário" onPress={() => navigation.navigate('Usuario')} />
-      <Button title="Entrada" onPress={() => navigation.navigate('Entrada')} />
-      <Button title="Saída" onPress={() => navigation.navigate('Saida')} />
       <Button title="Lote" onPress={() => navigation.navigate('Lote')} />
     </View>
   );
